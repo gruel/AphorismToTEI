e8596627e70c8b401108a2edc9500201292b8e6c
Jonathan Boyle <jonathan3145@gmail.com>
2016-01-06 16:20:31 +0000

    This is the first commit. 1. CommentaryToEpidoc.py contains a Python module which has been written    to convert text files containing certain symbols (defined in the module    documentation) into EpiDoc compatible XML. 2. TextFiles/collation_Book_3.txt is an example input file. This file has    been fabricated for testing and may contain garbage. 3. xml_template.txt is an input file required by the module 4. driver.py can be used to convert TextFiles/collation_Book_3.txt    into XML.

---
+++ driver.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 14:14:17 2015
+
+@author: mcbicjb2
+"""
+
+import imp
+import CommentaryToEpidoc
+
+# We need to reload if the package code has been modified
+imp.reload(CommentaryToEpidoc)
+
+# Call ArabicToXML.process_text_files with the following arguements
+# 1st - the folder containing the text file
+# 2nd - the name of the XML template file
+# 3rd - the number of offsets to use when adding XML to the <body> element
+# 4th - the number of space characters to use for each XML offset
+
+location_of_files = "./TextFiles"
+template_file = "xml_template.txt"
+n_offsets = 3
+n_space = 4
+
+CommentaryToEpidoc.process_text_files(location_of_files, \
+                               template_file, \
+                               n_offsets, \
+                               n_space)
+    
+print("Finished")
