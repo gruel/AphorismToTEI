a1c1a6466ba7c6d49fec6110d5f4fd3a06c682de
Nicolas Gruel <nicolas.gruel@manchester.ac.uk>
2017-01-18 14:55:48 +0000

    Correct installation script to use the correct name for the package.
    Bug solved

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
+#TODO: add data in the installation for the default xml_template.txt
+
 from setuptools import setup
 
 setup(name='hyppocratic',
       version='0.1',
       description='Software to convert text files to EpiDoc compatible XML.',
       author='Johathan Boyle, Nicolas Gruel',
-      packages=['CommentaryToEpidoc'],
+      packages=['hyppocratic'],
       install_requires=['docopt'],
       entry_points={
           'console_scripts': [
